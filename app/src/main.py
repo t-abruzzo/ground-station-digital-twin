265a1d759a0b044d21d440919fcd2bf690b51a5a
t-abruzzo <aabruzzo17@gmail.com>
2026-08-19 11:21:36 -0400

    Add Python service health check

--- app/src/main.py
+++ app/src/main.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python3
 
-config = {}
+import subprocess
 
-with open("app/config/settings.conf", "r") as file:
-    for line in file:
-        line = line.strip()
+def check_service(service_name):
+    result = subprocess.run(
+        ["systemctl", "is-active", "sshd"],
+        capture_output=True,
+        text=True
+    )
 
-        if line and not line.startswith("#"):
-            key, value = line.split("=", 1)
-            config[key] = value
+    return result.stdout.strip() == "active"
 
-print(f"Application: {config['APP_NAME']}")
-print(f"Environment: {config['ENVIRONMENT']}")
-print(f"Log Level: {config['LOG_LEVEL']}")
-print("Ground Station Simulator starting...")
+def load_config():
+    config = {}
+
+    with open("app/config/settings.conf", "r") as file:
+        for line in file:
+            line = line.strip()
+
+            if line and not line.startswith("#"):
+                key, value = line.split("=", 1)
+                config[key] = value
+
+    return config
+
+config = load_config()
+
+def show_status(online):
+    if online:    
+        print("GROUND STATION STATUS: ONLINE")
+    else:
+        print("GROUND STATION STATUS: OFFLINE")
+
+
+def display_config(config):
+    print(f"Application: {config['APP_NAME']}")
+    print(f"Environment: {config['ENVIRONMENT']}")
+    print(f"Log Level: {config['LOG_LEVEL']}")
+    print("Ground Station Simulator starting...")
+
+config = load_config()
+display_config(config)
+online = check_service("sshd")
+show_status(online)
 
